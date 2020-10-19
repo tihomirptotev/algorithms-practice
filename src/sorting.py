9979a29b451134dc060c973fd852aa29a7abeecd
Tihomir Totev <tihomirptotev@gmail.com>
2020-10-19 15:47:22 +0300

    Update .gitignore

--- src/sorting.py
+++ src/sorting.py
@@ -1,22 +1,15 @@
 from typing import List, Sequence
 
 
-def bubble_sort(input_array: Sequence) -> List:
-    arr = list(input_array)
-
+def bubble_sort(arr: List):
     last_index = len(arr) - 1
-
     for c in range(last_index, 0, -1):
         for i in range(c):
             if arr[i] > arr[i + 1]:
                 arr[i], arr[i + 1] = arr[i + 1], arr[i]
 
-    return arr
 
-
-def insertion_sort(input_array: Sequence) -> List:
-    arr = list(input_array)
-
+def insertion_sort(arr: List):
     for i in range(1, len(arr)):
         current = arr[i]
         position = i
@@ -26,3 +19,7 @@ def insertion_sort(input_array: Sequence) -> List:
         arr[position] = current
 
     return arr
+
+
+def merge_sort(input_list: List):
+    pass
--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -3,18 +3,18 @@ from src import sorting
 
 
 @pytest.mark.parametrize(
-    "input_array", [(1, 9, 2, 5, 9, 3, 7, 16, 10, 8), ["a", "f", "d", "c"], []],
+    "input_array", [[1, 9, 2, 5, 9, 3, 7, 16, 10, 8], ["a", "f", "d", "c"], []],
 )
 def test_bubble_sort(input_array):
-    arr = sorting.bubble_sort(input_array)
-    assert isinstance(arr, list)
-    assert arr == sorted(input_array)
+    expected = sorted(input_array)
+    sorting.bubble_sort(input_array)
+    assert input_array == expected
 
 
 @pytest.mark.parametrize(
-    "input_array", [(1, 9, 2, 5, 9, 3, 7, 16, 10, 8), ["a", "f", "d", "c"], []],
+    "input_array", [[1, 9, 2, 5, 9, 3, 7, 16, 10, 8], ["a", "f", "d", "c"], []],
 )
 def test_insertion_sort(input_array):
-    arr = sorting.insertion_sort(input_array)
-    assert isinstance(arr, list)
-    assert arr == sorted(input_array)
+    expected = sorted(input_array)
+    sorting.insertion_sort(input_array)
+    assert input_array == expected
